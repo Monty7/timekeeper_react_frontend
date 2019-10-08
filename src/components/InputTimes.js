42aed4a64dd536d175b051ceb2a7f472ee3238c6
github username <github email address>
2019-10-08 13:07:57 -0500

    Added events to inputs for clockin and clock out

--- src/components/InputTimes.js
+++ src/components/InputTimes.js
@@ -21,9 +21,9 @@ export default class InputTimes extends Component{
             <React.Fragment>
                 <span>01</span>
                 <label>Clock In</label>
-                <input type="time" name="clockIn"placeholder="Clock In" class="clockIn" />
+                <input type="time" name="clockIn"placeholder="Clock In" class="clockIn" onChange={(event) => this.handleOnChange(event)} />
                 <label>Clock Out</label>
-                <input type="time" name="clockOut" placeholder="Clock Out" class="clockOut" />
+                <input type="time" name="clockOut" placeholder="Clock Out" class="clockOut" onChange={(event) => this.handleOnChange(event)} />
 
             <div>
                 <AddButton />
