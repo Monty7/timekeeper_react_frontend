ec749bfe268c2cd2a974b66b0782ec72068d1aa1
github username <github email address>
2019-10-21 16:50:15 -0500

    Changed the way the state and props are handled in my login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,38 +1,18 @@
-import React, {Component} from 'react'
+import React from 'react'
 import LoginButton from './buttons/LoginButton'
 import Header from './Header'
-import {fetchUser} from '../actions/login'
-import { connect } from 'react-redux'
-import { withRouter } from "react-router-dom"
-import {checkForUser} from '../actions/login'
 
 
-class LoginForm extends Component{
-    state = {
-        name: ''
-    }
 
-    handleOnChange(event){
-      
-        this.setState({
-            [event.target.name]: event.target.value
-        }) 
-    }
-    
-    handleSubmit(event){
-        event.preventDefault()
-        this.props.fetchUser({name: this.state.name}, this.props.history)
-    }
+const LoginForm = props => {
 
-render(){
-    
     return(
         <div>
             <Header />
-            <form onSubmit={(event) => this.handleSubmit(event) }>
+            <form onSubmit={(event) => props.submitValue(event) }>
                 <div id="sign_up_in">
                 <div id="sign_up">
-                    <input type="text" name="name" id="sign_in_user" placeholder="Log In" onChange={(event) => this.handleOnChange(event)} />
+                    <input type="text" name="name" id="sign_in_user" placeholder="Log In" onChange={(event) => props.inputValue(event)} />
                     <LoginButton id="submit_user" />
             
                 </div>
@@ -40,11 +20,7 @@ render(){
             </form>
         </div>
         )
-    }
 }
 
-const mapStateToProps = (state) => ({
-    user: state.user //user is the key from the reducer
-    //will be able to utilize this.props.user to display in the form
-})
-export default connect(mapStateToProps, {fetchUser})(withRouter(LoginForm))
+export default LoginForm
+
--- src/containers/LoginFormContainer.js
+++ src/containers/LoginFormContainer.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react'
 import LoginForm from '../components/LoginForm'
+import {fetchUser} from '../actions/login'
+import { connect } from 'react-redux'
+import { withRouter } from "react-router-dom"
 
 class LoginFormContainer extends Component{
+    state = {
+        name: ''
+    }
+
+    handleOnChange = (event) => {
+      
+        this.setState({
+            [event.target.name]: event.target.value
+        }) 
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.fetchUser({name: this.state.name}, this.props.history)
+    }
 
 
     render(){
         return(
             
-                <LoginForm />
+                <LoginForm formData={this.state} inputValue={this.handleOnChange} submitValue={this.handleSubmit}/>
            
         )
     }
 }
+const mapStateToProps = (state) => ({
+    user: state.user //user is the key from the reducer
+    //will be able to utilize this.props.user to display in the form
+})
+export default connect(mapStateToProps, {fetchUser})(withRouter(LoginFormContainer))
 
-
-export default LoginFormContainer
