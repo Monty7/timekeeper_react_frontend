a5eb1a7d0fbbd43617facd48980078e7b197ec6e
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2019-12-11 03:20:55 -0600

    Changed base url from local to server

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,5 @@
-const BASEURL = 'http://localhost:3000'
+//const BASEURL = 'http://localhost:3000'
+const BASEURL = 'https://time-keeper-api.herokuapp.com'
 //const USER_URL = "http://localhost:3000/users";
 
 export default BASEURL
