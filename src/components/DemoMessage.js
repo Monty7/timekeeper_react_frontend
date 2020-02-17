e6b50fb4f80adaacf17dbc1124649a94db7c77b3
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-02-16 22:09:19 -0600

    Removed a line in the Demo message component.

--- src/components/DemoMessage.js
+++ src/components/DemoMessage.js
@@ -4,7 +4,6 @@ const DemoMessage = () => {
     return (
         <footer>
             <p>DEMO PURPOSES ONLY</p>
-            <p>Note: Entering a real name is not necessary.</p>
         </footer>
     )
 }
